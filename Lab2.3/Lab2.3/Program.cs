21e1a85aa1d3b441117f17c08ea01b2d38ef40dc
Denis Tveritinof <deneek202@gmail.com>
2026-04-20 21:59:13 +0300

    Finished_lab2.3

---
+++ Lab2.3/Lab2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static long CalculateArrangements(int n, int k)
+    {
+        long result = 1;
+
+        for (int i = 0; i < k; i++)
+        {
+            result *= (n - i);
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 11;
+        int k = 4;
+
+        long result = CalculateArrangements(n, k);
+
+        Console.WriteLine($"{"Кількість учасників",-20} | {n}");
+        Console.WriteLine($"{"Кількість у команді",-20} | {k}");
+        Console.WriteLine(new string('-', 30));
+        Console.WriteLine($"{"Варіантів складу",-20} | {result}");
+
+        Console.ReadKey();
+    }
+}
