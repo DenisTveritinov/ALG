015e5563461b916b33a78d91efc3f9c2d64cf980
Denis Tveritinof <deneek202@gmail.com>
2026-04-20 21:48:25 +0300

    finished_lab2.1

---
+++ Lab2.1/Lab2.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static double f(double x)
+    {
+        return x * Math.Sqrt(2 * x + 1);
+    }
+
+    static void Main()
+    {
+        double a = 0;
+        double b = 2;
+        double h = 1.0;
+        int n = (int)((b - a) / h);
+
+        double[] x = new double[n + 1];
+        double[] y = new double[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            x[i] = a + i * h;
+            y[i] = f(x[i]);
+        }
+
+        //Метод прямокутників
+        double rect = 0;
+        for (int i = 0; i < n; i++) rect += y[i];
+        rect *= h;
+
+        //Метод трапецій
+        double trap = h * ((y[0] + y[n]) / 2);
+        for (int i = 1; i < n; i++) trap += h * y[i];
+
+        //Метод Сімпсона
+        double simp = (h / 3) * (y[0] + 4 * y[1] + y[2]);
+
+        Console.WriteLine($"Rectangles: {rect:F5}");
+        Console.WriteLine($"Trapezoids: {trap:F5}");
+        Console.WriteLine($"Simpson: {simp:F5}");
+
+        Console.ReadKey();
+    }
+}
